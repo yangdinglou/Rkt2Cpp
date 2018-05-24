69c628b7804adc3c298869eead5bf5d3b5c9ac51
yangdinglou <1079179461@qq.com>
2018-05-24 11:59:01 +0800

    Format the code

--- test/lambda.cpp
+++ test/lambda.cpp
@@ -1,30 +1,31 @@
 #include "rkt2cpp.h"
-class Y
-{
+
+class Y {
 public:
-    auto operator()()
-    {
-        return [=](auto b){return [=](auto f){return b([=](auto x){return f(f)(x);});}([=](auto f){return b([=](auto x){return f(f)(x);});});};
+    auto operator()() {
+        return [=](auto b) {
+            return [=](auto f) { return b([=](auto x) { return f(f)(x); }); }(
+                    [=](auto f) { return b([=](auto x) { return f(f)(x); }); });
+        };
     }
-}Y;
-class F
-{
+} Y;
+
+class F {
 public:
-    auto operator()()
-    {
-        return [=](auto f){return [=](auto n){if(n==0)
-        {
-            return 1;
-        }
-        else
-        {
-            return mul(n, f(sub(n, 1)));
-        }
-        };};
+    auto operator()() {
+        return [=](auto f) {
+            return [=](auto n) {
+                if (n == 0) {
+                    return 1;
+                } else {
+                    return mul(n, f(sub(n, 1)));
+                }
+            };
+        };
     }
-}F;
-int main()
-{
+} F;
+
+int main() {
     if (!is_void<decltype(Y()(F())(10))>::value) cout << Y()(F())(10) << '\n';
     return 0;
 }
